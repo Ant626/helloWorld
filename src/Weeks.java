ac147aa48b1da0f6e12cd768f9945cc2ee6c99df
Anthony W <45855928+Ant626@users.noreply.github.com>
2022-06-14 15:50:29 -0400

    weeks

---
+++ src/Weeks.java
@@ -0,0 +1,40 @@
+    import java.util.Scanner;
+
+public class Weeks {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Please enter number of weeks:" );
+        int numberOfWeeks = input.nextInt();
+        int days = numberOfWeeks * 7;
+        int hours = 24;
+        int minutes = 60;
+        int seconds = 60;
+
+
+
+
+        //Number of Days
+        System.out.println("Days " + days);
+
+
+        //Number of Hours
+        System.out.println("Hours: " + days * hours);
+
+        //Number of Minutes
+        System.out.println("Minutes: " + days * hours * minutes);
+
+        // Number of Seconds
+        System.out.println("Seconds: " + days * hours * minutes * seconds);
+
+
+
+
+    }
+
+
+
+
+
+}
